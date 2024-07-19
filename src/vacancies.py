6a55628c759bb7d5c3a15ff010b30a70a629e2ed
RealmDragon <realmdragon2014@yandex.ru>
2024-07-19 19:01:08 +0300

    удалина папка idea и вединных исправления в Атрибутах

--- src/vacancies.py
+++ src/vacancies.py
@@ -4,20 +4,20 @@ class Vacancies:
     """ Класс для обработки вакансии """
 
     def __init__(self):
-        self.__all_vacancies = []
+        self.all_vacancies = []
 
     def add_vacancies(self, new_vacancies):
-        self.__all_vacancies += new_vacancies
+        self.all_vacancies += new_vacancies
 
     @property
     def all_vacancies(self):
-        return self.__all_vacancies
+        return self.all_vacancies
 
     def to_list_dict(self):
         list_dict = []
-        for i in self.__all_vacancies:
+        for i in self.all_vacancies:
             list_dict.append(i.vacancies_dict())
         return list_dict
 
     def sorted_vacancy_by_salary(self):
-        self.__all_vacancies.sort(reverse=True)
+        self.all_vacancies.sort(reverse=True)
